40442a53c13e4b7ff4b069d249edb44522cfd16d
Jessica Black <me@jessica.black>
2022-12-12 19:17:16 -0800

    Simplify main

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,24 @@ use stable_eyre::Report;
 
 mod day1;
 mod day2;
+mod day3;
+
+macro_rules! run_day {
+    ($day:ident) => {{
+        println!("== {} ==", stringify!($day));
+        println!(" part1: {}", $day::part1($day::INPUT)?);
+        println!(" part2: {}", $day::part2($day::INPUT)?);
+        println!();
+    }};
+}
 
 fn main() -> Result<(), Report> {
     println!("advent of code 2022");
     println!();
 
-    println!("Day 1:");
-    println!("- Part 1: {}", day1::part1(day1::INPUT)?);
-    println!("- Part 2: {}", day1::part2(day1::INPUT)?);
-    println!();
-
-    println!("Day 2:");
-    println!("- Part 1: {}", day2::part1(day2::INPUT)?);
-    println!("- Part 2: {}", day2::part2(day2::INPUT)?);
-    println!();
+    run_day!(day1);
+    run_day!(day2);
+    run_day!(day3);
 
     Ok(())
 }
